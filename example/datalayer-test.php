c7f3b49e38f0cc5397cab8094bfe12a1ba911efb
Mehul Gohil <mehul.gohil@get10up.com>
2022-10-29 10:32:13 +0530

    add example files

---
+++ example/datalayer-test.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: 10up's Datalayer - Test Plugin
+ * Version: 1.0.0
+ * Author: 10up
+ * Author URI: https://10up.com
+ */
+
+namespace DataLayer\Test;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Autoload using vendor directory via composer.
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * 
+ */
+function header_section() {
+    // Initiate and setup Datalayer.
+    $datalayer = ( new \TenUp\DataLayer\Datalayer() )->setup( 'json' );
+    
+    // Use `$datalayer` parameters based on your need.
+}
+
+add_action( 'wp_head', __NAMESPACE__ . '\header_section' );
+
+/**
+ * Enqueue Scripts.
+ * 
+ * @since 1.0.0
+ * 
+ * @return void
+ */
+function enqueue_scripts() {
+    // Initiate and setup Datalayer.
+    $datalayer = ( new \TenUp\DataLayer\Datalayer() )->setup( 'array' ); // `array` is the default value.
+
+    // Enqueue and Localize script where you would like to use the `$datalayer` parameters.
+    wp_enqueue_script( 'datalayer-test', plugins_url( '/ad-datalayer/test.js' ) );
+    wp_localize_script( 'datalayer-test', 'datalayerArgs', $datalayer );
+}
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts' ); 
+
+
