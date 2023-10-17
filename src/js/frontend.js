180f1fb3a0e9150000596b3362a253d622a22128
Gary Thayer <gary.thayer@10up.com>
2023-10-16 20:48:26 -0400

    bind the GTM click events

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,3 +1,34 @@
+/**
+ * Send the event to Google Tag Manager.
+ *
+ * @param {HTMLElement} element The element that was clicked.
+ */
+function sendGTMEvent(element) {
+	const attributeList = ['data-event', 'data-ctaText', 'data-destinationLink'];
+
+	const data = attributeList.reduce((acc, attribute) => {
+		const value = element.getAttribute(attribute);
+		if (value) {
+			const key = attribute.replace('data-', '');
+			acc[key] = value;
+		}
+		return acc;
+	}, {});
+
+	window.dataLayer = window.dataLayer || [];
+	window.dataLayer.push({ data });
+}
+
+/**
+ * Add event listeners to the document.
+ */
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('DOMContentLoaded');
+	document.addEventListener('click', function (event) {
+		const clickedElement = event.target;
+		const dataEvent = clickedElement.getAttribute('data-event');
+
+		if (dataEvent) {
+			sendGTMEvent(clickedElement);
+		}
+	});
 });
