a1cf5d2f867ff40cd670110782c2ef82e3168e81
Gary Thayer <gary.thayer@10up.com>
2023-11-06 17:22:54 -0500

    update examples

--- example/datalayer-test.php
+++ example/datalayer-test.php
@@ -17,40 +17,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . '/vendor/autoload.php';
 
 /**
+ * Add GTM ID.
+ */
+function add_gtm_id() {
+	return 'GTM-XXXXXXX';
+}
+add_filter( 'tenup_datalayer_gtm_id', __NAMESPACE__ . 'add_gtm_id' );
+
+/**
  * Enqueue the block data class.
  *
  * @return void
  */
-function add_block_data() {
-	new \TenUp\DataLayer\BlockData();
+function add_datalayer_classes() {
+	new \TenUp\DataLayer\Blockdata();
+	new \TenUp\DataLayer\Datalayer();
 }
-add_action( 'wp_loaded', __NAMESPACE__ . 'add_block_data' );
-
-
-/**
- * 
- */
-function header_section() {
-    // Initiate and setup Datalayer.
-    $datalayer = ( new \TenUp\DataLayer\Datalayer() )->setup( 'json' );
-    
-    // Use `$datalayer` parameters based on your need.
-}
-add_action( 'wp_head', __NAMESPACE__ . '\header_section' );
-
-/**
- * Enqueue Scripts.
- * 
- * @since 1.0.0
- * 
- * @return void
- */
-function enqueue_scripts() {
-    // Initiate and setup Datalayer.
-    $datalayer = ( new \TenUp\DataLayer\Datalayer() )->setup( 'array' ); // `array` is the default value.
-
-    // Enqueue and Localize script where you would like to use the `$datalayer` parameters.
-    wp_enqueue_script( 'datalayer-test', plugins_url( '/ad-datalayer/test.js' ) );
-    wp_localize_script( 'datalayer-test', 'tenupDataLayer', $datalayer );
-}
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts' ); 
+add_action( 'wp_loaded', __NAMESPACE__ . '\wp_loaded' );
